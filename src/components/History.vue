47b2e16a57dbe06e2393bbf41991822ac7ff5b89
Keith Wade <keithwade@nanonation.net>
2016-11-29 14:20:24 -0600

    Fix styling and update History

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,6 +1,18 @@
-<template>
+<template v-if='$store.state.history.length > 0'>
   <div class="history">
-    <div v-for='pokemon in $store.state.history'>{{pokemon.name}}</div>
+    <div class='ui four column stackable grid container'>
+      <div class='column' v-for='pokemon in $store.state.history'>
+        <div class='ui card'>
+          <div class='image'>
+            <img src='' v-bind:alt='pokemon.name' />
+          </div>
+          <div class='content'>
+            <div class='header'>{{pokemon.id}} - {{pokemon.name}}</div>
+            <div class='description'>{{pokemon.types}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,37 +1,36 @@
 <template>
   <div class="viewer">
-    <div className='ui segments'>
-      <div className='ui center aligned segment'>
-        <h1 className='ui huge header'>{{$store.state.current.id}} - {{$store.state.current.name}}</h1>
+    <div class='ui segments'>
+      <div class='ui center aligned segment'>
+        <h1 class='ui huge header'>{{$store.state.current.id}} - {{$store.state.current.name}}</h1>
       </div>
-      <div className='ui segment'>
-        <div className='ui two column divided stackable grid'>
-          <div className='row'>
-            <div className='column'>
+      <div class='ui segment'>
+        <div class='ui two column divided stackable grid'>
+          <div class='row'>
+            <div class='column'>
               <img v-bind:src='url' v-bind:alt='$store.state.current.name' />
             </div>
-            <div className='column'>
-              <h3 className='ui dividing header'>Type</h3>
+            <div class='column'>
+              <h3 class='ui dividing header'>Type</h3>
+                {{$store.state.current.types}}
+              <h3 class='ui dividing header'>Weaknesses</h3>
               <div v-for='pokeType in $store.state.current.types'>
               </div>
-              <h3 className='ui dividing header'>Weaknesses</h3>
+              <h3 class='ui dividing header'>Resistances</h3>
               <div v-for='pokeType in $store.state.current.types'>
               </div>
-              <h3 className='ui dividing header'>Resistances</h3>
-              <div v-for='pokeType in $store.state.current.types'>
-              </div>
-              <h3 className='ui dividing header'>Immunities</h3>
+              <h3 class='ui dividing header'>Immunities</h3>
               <div v-for='pokeType in $store.state.current.types'>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='ui center aligned segment'>
+      <div class='ui center aligned segment'>
         Fork me on <a href='https://github.com/keawade/pokedata' target='_blank'>GitHub</a>
       </div>
-      <div v-if='$store.state.history.length > 0' className='ui divider' />
     </div>
+    <div v-if='$store.state.history.length > 0' class='ui divider' />
   </div>
 </template>
 
